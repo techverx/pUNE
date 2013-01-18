e96388621e8d8849c9d27af1d2405e9a3fca63f1
Arfeen Arif <arfeen@pwoxisolutions.com>
2013-01-18 17:25:12 +0500

    Twitter bootstrap integrated with jQuery.

---
+++ README.md
@@ -0,0 +1,11 @@
+pUNE
+====
+
+Play ground for different Node.js scripts and social integration. 
+
+Features
+========
+
+* Using express.js
+* Using Twitter Bootstrap with Jade templates and layouts.
+* jQuery is added for exciting tasks.
--- app.js
+++ app.js
@@ -4,32 +4,39 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+, routes = require('./routes');
 
-var app = express();
+var app = module.exports = express();
+
+// Configuration
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', {
+        layout: false
+    });
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler({
+        dumpExceptions: true, 
+        showStack: true
+    }));
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+    console.log("app running now...");
 });
